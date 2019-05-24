41cdcde4159c0092842f50a6bf1ac3bfb3c0c867
Jillian <jillianlwerner@gmail.com>
2019-05-24 00:57:30 -0400

    Add edit and delete actions to Villagers

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -2,5 +2,6 @@ class ApplicationController < ActionController::Base
   protect_from_forgery with: :exception
 
   def index
+    render html: "Welcome to the in-progress Stardew Valley app!"
   end
 end
--- app/controllers/villagers_controller.rb
+++ app/controllers/villagers_controller.rb
@@ -7,7 +7,7 @@ class VillagersController < ApplicationController
   end
 
   def create
-    @villager = Villager.new(article_params)
+    @villager = Villager.new(villager_params)
 
     @villager.save
     redirect_to @villager
@@ -17,8 +17,26 @@ class VillagersController < ApplicationController
     @villager = Villager.find(params[:id])
   end
 
+  def edit
+    @villager = Villager.find(params[:id])
+  end
+
+  def update
+    @villager = Villager.find(params[:id])
+
+    case params[:submit]
+    when "edit"
+      @villager.update_attributes(villager_params)
+      @villager.save
+      redirect_to @villager
+    when "delete"
+      @villager.destroy
+      redirect_to action: 'index'
+    end
+  end
+
   private
-    def article_params
+    def villager_params
       params.require(:villager).permit(:name)
     end
 end
