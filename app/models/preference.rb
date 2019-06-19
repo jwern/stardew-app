b8431a5fe3e152590e71540b7de8acb3a77b78f4
Jillian <jillianlwerner@gmail.com>
2019-06-19 00:28:39 -0400

    Preference validation update

--- app/models/preference.rb
+++ app/models/preference.rb
@@ -9,6 +9,6 @@ class Preference < ApplicationRecord
 
   validates_uniqueness_of :villager_id, scope: :item_id, message: "preference is already set"
   validates :opinion, presence: true, inclusion: { in: OPINIONS }
-  validates :item, inclusion: { in: Item.all }
-  validates :villager, inclusion: { in: Villager.all }
+  validates :item_id, inclusion: { in: Item.all }
+  validates :villager_id, inclusion: { in: Villager.all }
 end
