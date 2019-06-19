1de1f98a02259a4d17f1423e3c1362ba6c9d4295
Jillian <jillianlwerner@gmail.com>
2019-06-19 00:46:09 -0400

    Remove item and villager validations in preferences model

--- app/models/preference.rb
+++ app/models/preference.rb
@@ -8,7 +8,7 @@ class Preference < ApplicationRecord
   ]
 
   validates_uniqueness_of :villager_id, scope: :item_id, message: "preference is already set"
-  validates :opinion, presence: true, inclusion: { in: OPINIONS }
-  validates :item_id, inclusion: { in: Item.all }
-  validates :villager_id, inclusion: { in: Villager.all }
+  validates :opinion, inclusion: { in: OPINIONS }
+  #validates :item_id, inclusion: { in: Item.pluck(:id) }
+  #validates :villager_id, inclusion: { in: Villager.pluck(:id) }
 end
