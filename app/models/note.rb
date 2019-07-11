100f121510fe89728573c927efeb10ad7f578b82
Jillian <jillianlwerner@gmail.com>
2019-07-11 13:10:54 -0400

    Update home / notes page layout and table style

--- app/models/note.rb
+++ app/models/note.rb
@@ -1,2 +1,21 @@
 class Note < ApplicationRecord
+  NOTE_TYPES = [
+    "Farming",
+    "Mining",
+    "Relationships",
+    "General"
+  ]
+
+  def self.sort_by_type
+    note_types_hash = NOTE_TYPES.each_with_object({}) { |type, hash| hash[type] = [] }
+    Note.all.each { |n| note_types_hash[n.info_type] << n }
+    note_types_hash
+  end
 end
+
+##WORKS
+#note_types_hash = NOTE_TYPES.each_with_object({}) { |type, hash| hash[type] = [] }
+#Note.all.each { |n| note_types_hash[n.info_type] << n.additional_info }
+#note_types_hash
+
+#NOTE_TYPES.each { |type| note_types_hash[type] = [] }
