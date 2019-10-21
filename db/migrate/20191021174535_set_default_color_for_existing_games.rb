96c9bca01125477ac0dedc8c59438484c88f5147
Jillian <jillianlwerner@gmail.com>
2019-10-21 18:02:23 -0400

    Set up default value for Game colors

--- db/migrate/20191021174535_set_default_color_for_existing_games.rb
+++ db/migrate/20191021174535_set_default_color_for_existing_games.rb
@@ -1,6 +1,6 @@
 class SetDefaultColorForExistingGames < ActiveRecord::Migration[5.2]
   def up
-    Game.update_all(color_choice: "blue-red")
+    Game.update_all(color_choice: Game::COLORS.values.first)
   end
 
   def down
